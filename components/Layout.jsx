5cd78e218eb3336fee4b56dc5601712d6f918ab6
Sunny More <mxsunny10@gmail.com>
2022-08-27 16:46:36 +0530

    final commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,16 @@
-import Head from 'next/head';
-import { Box } from '@chakra-ui/react';
+import Head from "next/head";
+import { Box } from "@chakra-ui/react";
 
-import Footer from './Footer';
-import Navbar from './Navbar';
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Real Estate</title>
+        <title>Realtor</title>
       </Head>
-      <Box maxWidth='1280px' m='auto'>
+      <Box maxWidth="1280px" m="auto">
         <header>
           <Navbar />
         </header>
